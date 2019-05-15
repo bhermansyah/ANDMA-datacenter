a312ae6683a25f26b20ed63c52053c9fabeca71e
iMMAP <>
2019-05-15 13:25:40 +0430

    info.js update

--- geonode/static/lib/js/GeoExplorer.js
+++ geonode/static/lib/js/GeoExplorer.js
@@ -35369,8 +35369,8 @@ GeoExt.data.PrintProvider = Ext.extend(Ext.util.Observable, {
         if (!this.url) {
             return;
         }
-        // var url = this.url + "info.json";
-        var url = getServerUrl() + 'static/info.json';
+        var url = this.url + "info.json";
+        // var url = getServerUrl() + 'static/info.json';
         Ext.Ajax.request({
             url: url,
             method: "GET",
