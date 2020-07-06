9fa3004f956993091af14d0ec45db6f84dbd4a27
dodiws <dodiws@gmail.com>
2020-07-06 14:56:12 +0700

    fix print error on empty first name, last name

--- dashboard/views.py
+++ dashboard/views.py
@@ -179,7 +179,7 @@ def dashboard_detail(request):
 			# client.setPageMargins('1in', '1in', '1in', '1in')
 			client.setVerticalMargin("0.75in")
 			client.setHorizontalMargin("0.25in")
-			client.setHeaderUrl(str(domainpath) + 'static/'+v2_folder+'rep_header_vector.html?onpdf='+user_logo['onpdf']+'&userlogo='+user_logo['logo_url']+'&name='+request.user.first_name+' '+request.user.last_name+'&cust_title=&organization='+request.user.organization+'&isodate='+date_string+'&'+headerparam)
+			client.setHeaderUrl(str(domainpath) + 'static/'+v2_folder+'rep_header_vector.html?onpdf='+user_logo['onpdf']+'&userlogo='+user_logo['logo_url']+'&name='+request.user.first_name or ''+' '+request.user.last_name or ''+'&cust_title=&organization='+request.user.organization or ''+'&isodate='+date_string+'&'+headerparam)
 			# convert a web page and store the generated PDF to a variable
 			pdf = client.convertURI(str(domainpath)+'print?'+request.META.get('QUERY_STRING')+'&user='+str(request.user.id)+'&'+bodyparam)
 			 # set HTTP response headers
