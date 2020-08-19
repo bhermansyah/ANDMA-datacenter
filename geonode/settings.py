239bd29416b9d72c55e05408af0be87a5153fbb0
khalidprogrammer <khalid.csit1234@gmail.com>
2020-08-19 14:37:11 +0430

    change the footer contact detail

--- geonode/settings.py
+++ geonode/settings.py
@@ -493,7 +493,7 @@ ACCOUNT_APPROVAL_REQUIRED = False
 
 # Email for users to contact admins.
 # THEME_ACCOUNT_CONTACT_EMAIL = 'admin@example.com'
-THEME_ACCOUNT_CONTACT_EMAIL = 'afghanistan@immap.org'
+THEME_ACCOUNT_CONTACT_EMAIL = 'anhdc@andma.gov.af'
 
 #
 # Test Settings
