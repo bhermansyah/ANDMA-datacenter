30eb43816383a11eb8ef19efc7f158e3b5545541
dodiws <dodiws@gmail.com>
2020-09-30 16:13:34 +0700

    fix error 403 on geoexplorer when login using regular user

--- geonode/static/lib/js/GeoExplorer.js
+++ geonode/static/lib/js/GeoExplorer.js
@@ -51115,7 +51115,7 @@ gxp.Viewer = Ext.extend(Ext.util.Observable, {
     initGWCAvalaible : function(){
         var conn = new Ext.data.Connection;
         conn.request({
-          url: getServerUrl() + 'geoserver/gwc/rest/layers', //'/static/gwclayers.xml',
+          url: getServerUrl() + '/static/gwclayers.xml',
           scope:this,
           callback: function(options, success, response)
           {
